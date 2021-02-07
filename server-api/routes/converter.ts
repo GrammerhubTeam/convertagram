3d730fef25d5312c50e4698a6b605b5d901dab55
digipa <37922236+digipa@users.noreply.github.com>
2021-02-06 19:04:59 -0500

    Added time.ts file to data folder and update .gitignore to include package-lock.json and yarn.lock. (#1)

--- server-api/routes/converter.ts
+++ server-api/routes/converter.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from "express";
 import { lengthUnits } from "../data/length";
+import { timeUnits } from "../data/time";
 import { ConverterValue } from "../models/converter.model";
 
 /** Route that will be added to the express */
 const router = express.Router();
 const path: string = "/api/v1/converter";
-const units = {...lengthUnits,};
+const units = {...lengthUnits, ...timeUnits};
 
 // Define routes
 router.get(path, async (req: Request, res: Response) => {
