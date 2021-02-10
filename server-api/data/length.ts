b9bd23a8c843e5561f59cab49c18334f5828929d
andmiguedev <49592826+andmiguedev@users.noreply.github.com>
2021-02-09 21:39:01 -0500

    Add well known lengths to dictionary (#5)
    * Set factor value for English and International units
    
    * Done

--- server-api/data/length.ts
+++ server-api/data/length.ts
@@ -2,8 +2,56 @@ import { Units } from "../models/converter.model";
 
 const units: Units = {};
 
-units['m'] = {unit:'m', base: 'm', name: 'meter', type: 'length', factor: 1};
-units['cm'] = {unit:'cm', base: 'm', name: 'centimeter', type: 'length', factor: 0.01};
-units['mm'] = {unit:'mm', base: 'm', name: 'milimeter', type: 'length', factor: 0.001};
+units["yd"] = {
+  unit: "yd",
+  base: "m",
+  name: "yard",
+  type: "length",
+  factor: 1.0936,
+};
+units["mi"] = {
+  unit: "mi",
+  base: "m",
+  name: "mile",
+  type: "length",
+  factor: 0.00062137,
+};
+units["ft"] = {
+  unit: "ft",
+  base: "m",
+  name: "feet",
+  type: "length",
+  factor: 3.2808,
+};
+units["in"] = {
+  unit: "in",
+  base: "m",
+  name: "inch",
+  type: "length",
+  factor: 39.370,
+};
+
+units["km"] = {
+  unit: "km",
+  base: "m",
+  name: "kilometer",
+  type: "length",
+  factor: 1000.0,
+};
+units["m"] = { unit: "m", base: "m", name: "meter", type: "length", factor: 1 };
+units["cm"] = {
+  unit: "cm",
+  base: "m",
+  name: "centimeter",
+  type: "length",
+  factor: 0.01,
+};
+units["mm"] = {
+  unit: "mm",
+  base: "m",
+  name: "milimeter",
+  type: "length",
+  factor: 0.001,
+};
 
 export { units as lengthUnits };
