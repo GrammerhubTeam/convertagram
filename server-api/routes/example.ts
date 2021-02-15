2c2fab110fcf954baf3e866be11592c41e5800ac
Miguel Gomez <mgomez.developer@gmail.com>
2021-02-15 10:57:08 -0500

    Check if createExample method is correct

--- server-api/routes/example.ts
+++ server-api/routes/example.ts
@@ -7,15 +7,26 @@ const path: string = '/api/v1/example';
 
 // Define routes
 router.get(path, async (req: Request, res: Response) => {
-    return res.status(200).send('Got an example');
+  return res.status(200).send('Got an example');
 });
 
-router.post(path , async (req: Request, res: Response) => {
-    //TODO: ADD A METHOD TO GENERATE CREATED/MODIFIED INFO
-    const newExample: Example = {...req.body};
-    console.log(newExample)
-    return res.status(201).send(newExample);
+router.post(path, async (req: Request, res: Response) => {
+  //TODO: ADD A METHOD TO GENERATE CREATED/MODIFIED INFO
+  const createExample = (cookExample: Example): void => {
+    // Check if new conversion has been added
+    cookingUnits.push(cookExample);
+    console.log(cookingUnits);
+  };
+
+  // Set example for conversion
+  const cookingUnits: Example[] = [{ pound: '1' }, { ounces: '16' }];
+
+  const newExample: Example = { ...req.body };
+  createExample(newExample);
+  console.log(newExample);
+
+  return res.status(201).send(newExample);
 });
 
 // Export the route
-export { router as exampleRouter }
+export { router as exampleRouter };
